41e29908e3fa3e27e5496c0cba880a0ede2c3965
patorash <chariderpato@gmail.com>
2021-12-23 18:27:15 +0900

    refactor: 正規表現を使っていた箇所をend_with?を使うように修正した。

--- lib/ransacker_translator/controllers/concerns/format_ransack_parameters.rb
+++ lib/ransacker_translator/controllers/concerns/format_ransack_parameters.rb
@@ -4,14 +4,14 @@ module RansackerTranslator
   module FormatRansackParameters
     extend ActiveSupport::Concern
 
-    def format_ransack_params
-      if params[:q].present?
-        params[:q].keys.each do |key|
-          if key =~ /_all\z/ || key =~ /_any\z/
-            params[:q][key] = params[:q][key].split(' ') if params[:q][key].instance_of? String
+    private
+
+      def format_ransack_params
+        params[:q]&.each_key do |key|
+          if key.end_with?('_all', '_any') && params.dig(:q, key).instance_of?(String)
+            params[:q][key] = params.dig(:q, key).split(' ')
           end
         end
       end
-    end
   end
 end
