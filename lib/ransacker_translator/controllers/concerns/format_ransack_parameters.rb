b14ba00561aa459e524f5527ea5ea06a47b8c5d3
patorash <chariderpato@gmail.com>
2021-12-24 09:44:46 +0900

    fix: nilの場合にeachメソッドが呼ばれている不具合を修正した。

--- lib/ransacker_translator/controllers/concerns/format_ransack_parameters.rb
+++ lib/ransacker_translator/controllers/concerns/format_ransack_parameters.rb
@@ -7,7 +7,7 @@ module RansackerTranslator
     private
 
       def format_ransack_params
-        params[:q]&.keys.each do |key|
+        params[:q]&.keys&.each do |key|
           if key.end_with?('_all', '_any') && params.dig(:q, key).instance_of?(String)
             params[:q][key] = params.dig(:q, key).split(' ')
           end
