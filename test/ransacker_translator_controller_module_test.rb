ed76674d7210cf6927e92b268b2e622008fe8e43
patorash <chariderpato@gmail.com>
2021-12-24 09:45:28 +0900

    test: ransack用のパラメータがない場合に例外が起きないことを確認するテストを追加した

--- test/ransacker_translator_controller_module_test.rb
+++ test/ransacker_translator_controller_module_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class RansackerTranslatorControllerModuleTest < ActionController::TestCase
-  test '半角スペース区切りの文字列が配列に変換されていること' do
+  setup do
     @controller = DummyController.new
+  end
+
+  test '半角スペース区切りの文字列が配列に変換されていること' do
     hash = {
       q: { title_translate_cont_any: 'パーフェクトRuby みんなのGo言語 プログラミングTypeScript' }
     }
@@ -13,6 +16,15 @@ class RansackerTranslatorControllerModuleTest < ActionController::TestCase
     @controller.stub(:params, mock_params) do
       @controller.send(:format_ransack_params)
       assert_equal(expected_hash, @controller.params.to_unsafe_h)
+    end
+  end
+
+  test 'ransack用のパラメーターがない場合にエラーにならないこと' do
+    mock_params = ActionController::Parameters.new({})
+    @controller.stub(:params, mock_params) do
+      assert_nothing_raised do
+        @controller.send(:format_ransack_params)
+      end
     end
   end
 end
